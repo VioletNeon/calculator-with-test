94cdacdffe9fd11266ebbd773865740fc1affbf9
Куликов Валерий Романович <VRKulikov@beeline.ru>
2025-01-05 18:52:48 +0300

    Initial commit

---
+++ src/main/java/violet/neon/calculatorwithtest/CalculatorWithTestApplication.java
@@ -0,0 +1,13 @@
+package violet.neon.calculatorwithtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalculatorWithTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalculatorWithTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/violet/neon/calculatorwithtest/CalculatorWithTestApplicationTests.java
@@ -0,0 +1,13 @@
+package violet.neon.calculatorwithtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorWithTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
