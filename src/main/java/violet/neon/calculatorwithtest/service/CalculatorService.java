a705d2c13726ad67e28a62af2b8992c6fff0cb01
Куликов Валерий Романович <VRKulikov@beeline.ru>
2025-01-05 20:32:36 +0300

    Task-1

---
+++ src/main/java/violet/neon/calculatorwithtest/service/CalculatorService.java
@@ -0,0 +1,11 @@
+package violet.neon.calculatorwithtest.service;
+
+public interface CalculatorService {
+    int plus(int firstNum, int secondNum);
+
+    int minus(int firstNum, int secondNum);
+
+    int multiply(int firstNum, int secondNum);
+
+    float divide(int firstNum, int secondNum);
+}
---
+++ src/test/java/violet/neon/calculatorwithtest/CalculatorServiceImplTestConstants.java
@@ -0,0 +1,12 @@
+package violet.neon.calculatorwithtest;
+
+public class CalculatorServiceImplTestConstants {
+    public static final int ZERO = 0;
+    public static final int TWO = 2;
+    public static final int THREE = 3;
+    public static final float THREE_POINT_FIVE = 3.5f;
+    public static final int FOUR = 4;
+    public static final int FIVE = 5;
+    public static final int SIX = 6;
+    public static final int SEVEN = 7;
+}
